a8573ae23e1eff0b812942e2ea050010742bb47e
Fabrizio99 <avbar357159@gmail.com>
2020-12-17 16:22:15 -0500

    segunda parte

--- app/src/main/java/com/example/myexamuntels/InscripcionActivity.kt
+++ app/src/main/java/com/example/myexamuntels/InscripcionActivity.kt
@@ -2,10 +2,25 @@ package com.example.myexamuntels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_inscripcion.*
 
 class InscripcionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inscripcion)
+
+        button.setOnClickListener {
+            Toast.makeText(this.applicationContext,"Se registro con exito.",Toast.LENGTH_LONG).show()
+            editTextTextPersonName2.setText("")
+            editTextTextPersonName3.setText("")
+            editTextTextPersonName4.setText("")
+            editTextTextPersonName5.setText("")
+            editTextTextPersonName6.setText("")
+            editTextTextPersonName6.setText("")
+            editTextTextPersonName7.setText("")
+            editTextTextPersonName8.setText("")
+        }
+
     }
 }
