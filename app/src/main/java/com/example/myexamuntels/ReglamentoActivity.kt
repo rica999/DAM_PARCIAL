b73143394a1286b64b74bc24f5c28199ed6b0fdb
Rhisto Zea <rhisto_99_99@hotmail.com>
2020-12-17 17:49:10 -0500

    Zea Carhuas Rhisto

--- app/src/main/java/com/example/myexamuntels/ReglamentoActivity.kt
+++ app/src/main/java/com/example/myexamuntels/ReglamentoActivity.kt
@@ -2,10 +2,26 @@ package com.example.myexamuntels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_reglamento.*
+import kotlinx.android.synthetic.main.activity_taller.*
 
 class ReglamentoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reglamento)
+
+        val listTalleres = listOf<String>("Mantener la cámara encendida.","Mantener el micrófono apagado","Todas las preguntas por el chat","Puntualidad","Registre su formulario de asistencia","Prohibido el uso de lenguaje que incite al odio","No interrumpir con sonidos en plena exposición")
+        val adapter = AdapterTalleres(this, listTalleres)
+        rcv_reglamento.apply {
+            layoutManager = androidx.recyclerview.widget.LinearLayoutManager(context)
+            setHasFixedSize(false)
+            this.adapter = adapter
+            itemAnimator = androidx.recyclerview.widget.DefaultItemAnimator()
+        }
+
+        btn_atras2.setOnClickListener {
+            finish()
+        }
+
     }
 }
