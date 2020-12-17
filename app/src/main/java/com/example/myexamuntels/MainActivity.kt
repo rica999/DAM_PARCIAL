edc87351c399e04587c7279dc498e4014e9b66f8
angelhr <angelhuamannahui@gmail.com>
2020-12-17 15:28:58 -0500

    huamanñahui robles newton

--- app/src/main/java/com/example/myexamuntels/MainActivity.kt
+++ app/src/main/java/com/example/myexamuntels/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.myexamuntels
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        img_insc.setOnClickListener {
+            val intent = Intent(this, InscripcionActivity::class.java)
+            startActivity(intent)
+        }
+
+        img_taller.setOnClickListener {
+            val intent = Intent(this, TallerActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        img_regl.setOnClickListener {
+            val intent = Intent(this, ReglamentoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
